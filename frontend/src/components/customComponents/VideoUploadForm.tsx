40c8ad83a9c2b14c4c30c8f9ac324b5f19012a93
Gowtham <gajavijay8@gmail.com>
2024-12-09 00:25:36 +0530

    ui Changes for submit form

--- frontend/src/components/customComponents/VideoUploadForm.tsx
+++ frontend/src/components/customComponents/VideoUploadForm.tsx
@@ -22,19 +22,18 @@ export default function VideoUploadForm() {
   // 1. Define your form.
   const form = useForm<z.infer<typeof fileSchema>>({
     resolver: zodResolver(fileSchema),
+    defaultValues:null
   });
 
-  // 2. Define a submit handler.
   const [video, setVideo] = useState<string | null>(null);
-
+  const [clear,setClear] = useState<string | null>(null);
+// 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof fileSchema>) {
-    console.log(values.file);
     const formData = new FormData();
     formData.append("video", values.file);
     axios
       .post(`${BACKEND_URL}api/v1/upload`, formData)
       .then((res) => {
-        console.log(res.data);
         toast({
           title: "Sucess",
           description: "Video Uploaded sucess fully",
@@ -48,6 +47,10 @@ export default function VideoUploadForm() {
         });
         console.log(res.message);
       });
+    setVideo(null)
+    form.reset()
+    setClear(prev => prev + 1)
+
   }
   return (
     <div className="container w-10/12 mx-auto">
@@ -64,6 +67,7 @@ export default function VideoUploadForm() {
                 <FormLabel className="text-2xl">Video</FormLabel>
                 <FormControl>
                   <Input
+                    key={clear}
                     type="file"
                     name="file"
                     accept="video/mp4, video/mov"
@@ -71,9 +75,8 @@ export default function VideoUploadForm() {
                     onChange={(event) => {
                       field.onChange(event.target.files?.[0]);
                       if (event.target.files && event.target.files[0]) {
-                        console.log(event.target.files[0]);
                         const file = event.target.files[0];
-                        setVideo(URL.createObjectURL(file));
+                        setVideo(prev =>  URL.createObjectURL(file));
                       }
                     }}
                   />
